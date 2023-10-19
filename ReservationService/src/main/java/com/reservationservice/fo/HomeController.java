299fb8175cf152450f973de77e4c18e4ca2acaea
216 <216@109-2-28>
2023-10-19 11:59:21 +0900

    feat: push, pull test

--- ReservationService/src/main/java/com/reservationservice/fo/HomeController.java
+++ ReservationService/src/main/java/com/reservationservice/fo/HomeController.java
@@ -35,5 +35,18 @@ public class HomeController {
 		
 		return "home";
 	}
-	
+
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String main(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
 }
